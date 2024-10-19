ab256447387a8d90e53337cb1e76714ad0b1c2cc
Sanchit Lamba <121615638+sanchit-lamba@users.noreply.github.com>
2024-10-19 20:30:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
     try {
       for (const cin of cins) {
         if (cin.trim() !== '') {
-          const response = await axios.get(`http://20.197.35.82:8000/brsr-report/?cin=${cin}`, {
+          const response = await axios.get(`http://api.sanchit.cc:8000/brsr-report/?cin=${cin}`, {
             withCredentials: false
           });
           newReports.push(response.data.parsed_response);
